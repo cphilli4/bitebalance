ff240619adf63d72a9fbe41a00d69762f40c1702
cphilli4 <cphilli4@stevens.edu>
2024-10-18 03:01:07 -0400

    Update imageAnalyze.py

--- BiteBalanceBackend/app/tests/test_meal/imageAnalyze.py
+++ BiteBalanceBackend/app/tests/test_meal/imageAnalyze.py
@@ -1,5 +1,7 @@
 import base64
-
+import piexif
+import os
+from PIL import Image
 from openai import OpenAI
 
 class ImageRecipeExtractor:
@@ -7,12 +9,26 @@ class ImageRecipeExtractor:
         self.image_path = image_path
         self.client = OpenAI()
 
+    def shrink_image(self):
+        im = Image.open(self.image_path)
+        exif_dict = piexif.load(im.info["exif"])
+        exif_bytes = piexif.dump(exif_dict)
+        rgb_im = im.convert('RGB')
+        size_mb= os.path.getsize(self.image_path)>>20
+        width, height = rgb_im.size
+        while size_mb>=1:
+            size=int(width*0.75),int(height*0.75)
+            rez_image=rgb_im.resize(size)
+            rez_image.save(self.image_path,exif=exif_bytes)
+            size_mb=os.path.getsize(self.image_path)>>20
+
+
     def encode_image(self):
         with open(self.image_path, 'rb') as image:
             return base64.b64encode(image.read()).decode('utf-8')
 
     def get_recipe(self):
-        encoded_image = self.encode_image()
+        self.shrink_image()
         try:
             completion = self.client.chat.completions.create(
                 model="gpt-4o-mini",
